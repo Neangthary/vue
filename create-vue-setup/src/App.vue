402e40fd792c6ce8ad1d0f900e06e130dc4deb9d
Neang Thary <neangthary210@gmail.com>
2024-07-23 11:53:37 +0700

    v-for, looping, v-bind, v-on, Events and Methods

--- create-vue-setup/src/App.vue
+++ create-vue-setup/src/App.vue
@@ -4,8 +4,21 @@ export default {
   data() {
     return {
       name: 'John Doe',
-      status: 'pending',
+      status: 'active',
+      tasks: ['Task One', 'Task Two','Task Three'],
+      link: 'https://google.com'
     };
+  },
+  methods: {
+    toggleStatus(){
+      if (this.status === 'active') {
+        this.status = 'pending';
+      } else if (this.status === 'pending'){
+        this.status = 'inactive';
+      } else {
+        this.status = 'active';
+      }
+    },
   },
 };
 </script>
@@ -15,5 +28,20 @@ export default {
   <p v-if="status === 'active'">User is active</p>
   <p v-else-if="status === 'pending'">User is pending</p>
   <p v-else>User is inactive</p>
+
+  <h3>Tasks</h3>
+  <ul>
+    <li v-for="task in tasks" :key="task">{{ task }}</li>
+  </ul>
+  <!-- <a v-bind:href="link">Click for Google </a>  -->
+  <a :href="link">Click for Google </a> 
+  <!-- v-bind:href = :href -->
+
+  <br>
+  <!-- <button v-on:click="toggleStatus">Change Status</button> -->
+  <button @click="toggleStatus">Change Status</button>
+  <!-- v-on:click = @click -->
+
+
 </template>
 
