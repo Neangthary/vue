ced46dd9190ab65d16e66d4457d485722148dce2
Neang Thary <neangthary210@gmail.com>
2024-07-23 13:45:32 +0700

    Composition API

--- create-vue-setup/src/App.vue
+++ create-vue-setup/src/App.vue
@@ -1,30 +1,25 @@
+<script setup>
+import { ref } from 'vue';
 
-<script>
-export default {
-  data() {
-    return {
-      name: 'John Doe',
-      status: 'active',
-      tasks: ['Task One', 'Task Two','Task Three'],
-      link: 'https://google.com'
-    };
-  },
-  methods: {
-    toggleStatus(){
-      if (this.status === 'active') {
-        this.status = 'pending';
-      } else if (this.status === 'pending'){
-        this.status = 'inactive';
-      } else {
-        this.status = 'active';
-      }
-    },
-  },
+const name = ref('Thary');
+const status = ref('active');
+const tasks = ref(['Task One', 'Task Two', 'Task Three']);
+
+const toggleStatus = () => {
+  if (status.value === 'active') {
+    status.value = 'pending';
+  } else if (status.value === 'pending') {
+    status.value = 'inactive';
+  } else {
+    status.value = 'active';
+  }
 };
+
+
 </script>
 
 <template>
-  <h1>{{name}}</h1>
+  <h1>{{ name }}</h1>
   <p v-if="status === 'active'">User is active</p>
   <p v-else-if="status === 'pending'">User is pending</p>
   <p v-else>User is inactive</p>
@@ -33,9 +28,7 @@ export default {
   <ul>
     <li v-for="task in tasks" :key="task">{{ task }}</li>
   </ul>
-  <!-- <a v-bind:href="link">Click for Google </a>  -->
-  <a :href="link">Click for Google </a> 
-  <!-- v-bind:href = :href -->
+
 
   <br>
   <!-- <button v-on:click="toggleStatus">Change Status</button> -->
@@ -44,4 +37,3 @@ export default {
 
 
 </template>
-
